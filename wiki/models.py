cc6f065e515e6bd8950fe123fda0bd51332806dc
LucaNoah <luca.gfeller2@gmail.com>
2022-10-10 14:33:10 +0000

    in models.py add 2 database models, one for Articles, one for Comments

--- wiki/models.py
+++ wiki/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, 'Draft'), (1, 'Published'))
+
+
+class Article(models.Model):
+  title = models.CharField(max_length=200, unique=True)
+  slug = models.SlugField(max_length=200, unique=True)
+  author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wiki_article')
+  description = models.TextField(max_length=200)
+  content = models.TextField()
+  featured_image = CloudinaryField('image', default='placeholder')
+  created_date = models.DateTimeField(auto_now_add=True)
+  updated_date = models.DateTimeField(auto_now=True)
+  excerpt = models.TextField(blank=True)
+  status = models.IntegerField(choices=STATUS, default=0)
+  approved = models.BooleanField(default=False)
+
+  class Meta:
+    ordering = ['created_date']
+
+  def __str__(self):
+    return self.title
+
+
+class Comment(models.Model):
+  article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='comments')
+  name = models.CharField(max_length=80)
+  content = models.TextField()
+  created_date = models.DateTimeField(auto_now_add=True)
+  approved = models.BooleanField(default=False)
+
+  class Meta:
+    ordering = ["created_date"]
+
+  def __str__(self):
+    return f"Written{self.body} by {self.name}"
