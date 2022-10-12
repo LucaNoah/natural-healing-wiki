9361f7a18938f8cfa61e3e838221308b7ccc93bb
LucaNoah <luca.gfeller2@gmail.com>
2022-10-12 16:16:31 +0000

    Install django-summernote, in urls.py import include and register django_summernote urls, in admin.py import Atricle model create class ArticleAdmin to set summernote fields and some options to make the admin page more user friendly, migrate

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Article
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(SummernoteModelAdmin):
+
+  summernote_fields = ('description', 'content')
+  prepopulated_fields = {'slug': ('title',)}
+  list_filter = ('status', 'created_date', 'approved')
+  list_display = ('title', 'status', 'author','created_date')
+  search_fields = ['title', 'description', 'content']
+  actions = ['approve_article']
+
+  def approve_article(self, request, queryset):
+    queryset.update(approved=True)
