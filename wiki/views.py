5be3c479a28fc9dffc0eb4d71c12c7d22d455d8d
LucaNoah <luca.gfeller2@gmail.com>
2022-10-12 23:29:33 +0000

    In views.py import generic views & Article model create class ArticleList with Article as model & add some built in methods to render article list, in the wiki DIR create urls.py import views & django url path then create urlpattern for ArticleList view, in the naturalhealingwiki DIR in urls.py register wiki app url

--- wiki/views.py
+++ wiki/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Article
 
-# Create your views here.
+
+class ArticleList(generic.ListView):
+  model = Article
+  # , approved=True
+  queryset = Article.objects.filter(status=1).order_by('-created_date')
+  template_name = 'index.html'
+  paginate_by = 8
