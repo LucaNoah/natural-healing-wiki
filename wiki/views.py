cbd42ddfccf3f3efc051736eead8932461030ac2
LucaNoah <luca.gfeller2@gmail.com>
2022-10-17 11:12:28 +0000

    In templates DIR create article_detail extend from base.html and view article title, author, created_date, description, content, in views.py import View & get_object_or_404 & create class ArticleDetail to render article_detail.html, in urls.py create path for ArticleDetail

--- wiki/views.py
+++ wiki/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Article
 
 
 class ArticleList(generic.ListView):
   model = Article
-  # , approved=True
-  queryset = Article.objects.filter(status=1).order_by('-created_date')
+  queryset = Article.objects.filter(status=1, approved=True).order_by('-created_date')
   template_name = 'index.html'
   paginate_by = 8
+
+
+class ArticleDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Article.objects.filter(status=1, approved=True)
+        article = get_object_or_404(queryset, slug=slug)
+        comments = article.comments.filter(approved=True).order_by("-created_date")
+
+        return render(
+            request,
+            "article_detail.html",
+            {
+                "article": article,
+                "comments": comments,
+            },
+        )
